d0a4df6d9f7b8cf4bc09f11f49b6f000bfe0b471
Asha Karanam <asresha42@gmail.com>
2020-06-09 02:58:48 -0500

    updated file

--- WeekendActivityData.py
+++ WeekendActivityData.py
@@ -34,14 +34,18 @@ for a in range(1, 101):
     if a % 3 == 0 and a % 2 == 0:
         print(a)
 
+
 # 7. 	Write a program in Python to reverse a string and print only the vowel alphabet if exist in the string with their index.
 
-a = str(input("Enter any string:"))
-b = []
-for i in a:
-    if i in "aeiou":
-        b.append(i)
-print(b)
+def random(a):
+    b = a[::-1]
+    print(b)
+    for i in b:
+        if i in "aeiou":
+            print(i, "The index of the vowel is: ", b.index(i))
+
+
+print(random("consultadd"))
 
 
 # 8. 	Write a program in Python to iterate through the string “hello my name is abcde” and print the string which has even length of word.
@@ -64,12 +68,29 @@ for a in x:
         if a + b == 8:
             print("First Number:", a, "Second Number: ", b, "Result: ", (a + b))
 
+
 # 10. 	Write a program in Python to complete the following task:
 # Create two different list as in even_list and odd_list
 # Ask user to enter the number in the range of 1,50 and make sure if the entered number is even append it to the even_list and if the entered number is odd append it to the odd list.
 # Keep that in mind you can only add 5 items in each list
 # Make sure once you entered the total 5 element calculate the sum of the list and return the maximum out of the list.
 
+even_list = []
+odd_list = []
+while(True):
+  x = int(input('Enter any number: '))
+
+  if x%2 == 0:
+    even_list.append(x)
+  else:
+    odd_list.append(x)
+  if len(even_list) == 5 and len(odd_list) == 5:
+    print('Sum of even list: ', sum(even_list))
+    print('Even list maximum number: ', max(even_list))
+    print('Sum of odd list: ', sum(odd_list))
+    print('Odd list maximum number: ', max(odd_list))
+    break
+print("Cannot enter anymore numbers")
 
 # 11.Write a program to find out the occurrence of a specific word from an alphanumeric statement. Example: 12abcbacbaba344ab
 #                       Output: a=5 b=5 c=2 make sure you should avoid the numbers in you logic
